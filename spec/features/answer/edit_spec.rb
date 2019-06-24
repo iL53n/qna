513d8429f8806d78115e840d1997f680a4aaf9fd
iL53n <ilsegribko@gmail.com>
2019-06-24 21:21:45 +0300

    Only author can edit his answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-feature 'User can edit hie answer', %q{
+feature 'User can edit his answer', %q{
   In order to correct mistakes
   As an author of answer
   I'd like to be able to edit my answer
 } do
 
   given(:user) { create(:user) }
+  given(:user_not_author) { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:answer) { create(:answer, user: user, question: question) }
 
@@ -21,11 +22,11 @@ feature 'User can edit hie answer', %q{
       sign_in(user)
 
       visit question_path(question)
+
+      click_on 'Edit answer'
     end
 
     scenario 'edits his answer', js: true do
-      click_on 'Edit answer'
-
       within '.answers' do
         fill_in 'Body', with: 'edited answer'
         click_on 'Save'
@@ -36,10 +37,24 @@ feature 'User can edit hie answer', %q{
       end
     end
 
+    scenario 'edits his answer with errors', js: true do
+      within '.answers' do
+        fill_in 'Body', with: ''
+        click_on 'Save'
 
-    scenario 'edits his answer with errors'
-    scenario "tries to edit other user's question"
+        expect(page).to have_content answer.body
+        expect(page).to have_selector 'textarea'
+      end
+
+      expect(page).to have_content "Body can't be blank"
+    end
   end
 
+  scenario "tries to edit other user's question" do
+    sign_in(user_not_author)
 
+    visit question_path(question)
+
+    expect(page).to_not have_link 'Edit answer'
+  end
 end
