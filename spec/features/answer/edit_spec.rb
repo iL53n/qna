5bb630da116046055c2f954c189dd5313e669cf3
iL53n <ilsegribko@gmail.com>
2019-06-27 21:59:13 +0300

    User can edits his answer with attached files

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -48,6 +48,17 @@ feature 'User can edit his answer', %q{
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'edits his answer with attached files', js: true do
+      within '.answers' do
+        fill_in 'Body', with: 'edited answer'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   scenario "Authenticated user tries to edit other user's question" do
