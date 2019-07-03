47e51d39ada89db269acef13a1e2ad2d695796fc
iL53n <ilsegribko@gmail.com>
2019-07-03 09:23:27 +0300

    User can add multiple links in the question or answer.

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -9,11 +9,13 @@ feature 'User can add links to answer', %q{
   given(:question) { create(:question, user: user) }
   given(:gist_url) { 'https://gist.github.com/iL53n/0acd40b1345c1bafa854c6efb8a93a47' }
 
-  scenario 'User adds link when give an answer', js: true do
+  scenario 'User adds links when give an answer', js: true do
     sign_in(user)
     visit question_path(question)
 
     fill_in 'Your answer', with: 'Text answer'
+
+    click_on 'Add link'
 
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -7,13 +7,16 @@ feature 'User can add links to question', %q{
 } do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/iL53n/0acd40b1345c1bafa854c6efb8a93a47' }
+  given(:gist_url_second) { 'https://gist.github.com/iL53n/3c8114658970af2572879f69a0727cb0' }
 
-  scenario 'User adds link when asks question' do
+  scenario 'User adds links when asks question', js: true do
     sign_in(user)
     visit new_question_path
 
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
+
+    click_on 'Add link'
 
     fill_in 'Link name', with: 'My gist'
     fill_in 'Url', with: gist_url
