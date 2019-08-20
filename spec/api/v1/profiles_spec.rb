0f14b161d4fdd603d2e450f4de4575e22c90591c
iL53n <ilsegribko@gmail.com>
2019-08-20 22:15:35 +0300

    Add profiles api

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -25,15 +25,53 @@ describe 'Profiles API', type: :request do
         expect(response).to be_successful
       end
 
+      context 'user profile' do
+        it 'returns all public fields' do
+          %w[id email admin created_at updated_at].each do |attr|
+            expect(json[attr]).to eq me.send(attr).as_json
+          end
+        end
+
+        it 'does not return private fields' do
+          %w[password encrypted_password].each do |attr|
+            expect(json).to_not have_key(attr)
+          end
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/profiles' do
+    let(:api_path) { '/api/v1/profiles' }
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+    end
+
+    context 'authorized' do
+      let(:users) { create_list(:user, 4) }
+      let(:me) { users.first }
+      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+      before { get api_path, params: { access_token: access_token.token }, headers: headers }
+
+      it 'return 200 status' do
+        expect(response).to be_successful
+      end
+
+      it 'return all profiles but me' do
+        expect(json.size).to eq(users.size - 1)
+        expect(json).to_not include me
+      end
+
       it 'returns all public fields' do
         %w[id email admin created_at updated_at].each do |attr|
-          expect(json[attr]).to eq me.send(attr).as_json
+          expect(json.last[attr]).to eq users.last.send(attr).as_json
         end
       end
 
       it 'does not return private fields' do
         %w[password encrypted_password].each do |attr|
-          expect(json).to_not have_key(attr)
+          expect(json.last).to_not have_key(attr)
         end
       end
     end
