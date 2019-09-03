aaa9cd6cdf5ea10d5e8d7ea6fc9d50cc827cdaba
iL53n <ilsegribko@gmail.com>
2019-09-03 15:19:29 +0300

    Add search cancan

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -11,6 +11,8 @@ describe Ability do
     it { should be_able_to :read, Comment }
 
     it { should_not be_able_to :manage, :all }
+
+    it { should be_able_to :search, :all }
   end
 
   describe 'Admin' do
